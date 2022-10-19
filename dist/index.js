a39fb9c682cfd7d966e8f7e39c5be45f1631c2d8
Hugo Torres <hugo.torres@taxfyle.com>
2022-10-19 14:37:07 -0400

    debugging

--- dist/index.js
+++ dist/index.js
@@ -62,17 +62,42 @@ const core = __nccwpck_require__(66);
 const github = __nccwpck_require__(262);
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // get job statuses for the current workflow run
+    const token = core.getInput('github-token');
+
+    const octokit = github.getOctokit(token);
+    const context = github.context;
+
+    jobs = octokit.paginate(
+        {
+            ...context.repo,
+            'run_id': context.runId,
+        },
+    );
+
+    jobs.forEach(job => {
+        console.log(`${job}`);
+    });
+
+    // create slack payload
+
+    // instantiate slack API client
+
+    // make the call
+
+
 } catch (error) {
     core.setFailed(error.message);
 }
+
+    // // `who-to-greet` input defined in action metadata file
+    // const nameToGreet = core.getInput('who-to-greet');
+    // console.log(`Hello ${nameToGreet}!`);
+    // const time = (new Date()).toTimeString();
+    // core.setOutput("time", time);
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
 
 })();
 
